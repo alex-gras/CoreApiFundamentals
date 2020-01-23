136e7e45347bffab2e9a410fbef1bc512d47a1aa
AlexG <a.gras@avanade.com>
2020-01-23 16:13:32 +0100

    Versioning with HEADERS

--- src/Startup.cs
+++ src/Startup.cs
@@ -3,6 +3,7 @@ using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -20,6 +21,10 @@ namespace CoreCodeCamp
             opt.AssumeDefaultVersionWhenUnspecified = true;
             opt.DefaultApiVersion = new ApiVersion(1,1);
             opt.ReportApiVersions = true;
+            opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
+            //opt.ApiVersionReader = new QueryStringApiVersionReader("ver");
+
+
         });  
 
         services.AddMvc(opt => opt.EnableEndpointRouting = false)
